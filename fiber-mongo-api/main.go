bf4bb6a614a57fbf0ca1005e235549ddb6fc6cde
ronaldo <ronaldosilva@MacBook-Pro-de-ronaldo.local>
2023-10-16 23:42:35 -0300

    adicionado conexao com mongo

--- fiber-mongo-api/main.go
+++ fiber-mongo-api/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"fiber-mongo-api/configs"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
-	})
+	configs.ConnectDB()
 
 	app.Listen(":8080")
 }
