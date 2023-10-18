fadda4c02520bc6a0cbe6d67a1b4331ef0fe3ba7
ronaldo <ronaldosilva@MacBook-Pro-de-ronaldo.local>
2023-10-18 08:59:56 -0300

    adicionado o campo email na api

--- fiber-mongo-api/models/user_model.go
+++ fiber-mongo-api/models/user_model.go
@@ -6,5 +6,6 @@ type User struct {
 	Id       primitive.ObjectID `json:"id,omitempty"`
 	Name     string             `json:"name,omitempty" validate:"required"`
 	Location string             `json:"location,omitempty" validate:"required"`
+	Email    string             `json:"email,omitempty" validate:"required"`
 	Title    string             `json:"title,omitempty" validate:"required"`
 }
